e62b954384257a5f7c1e18741c9ef2687b460007
Frank <xd23fe39@yahoo.de>
2018-04-19 05:59:45 +0200

    Update java-run-appl.sh

--- java-bin/java-run-appl.sh
+++ java-bin/java-run-appl.sh
@@ -1,5 +1,17 @@
+# pastebin
+exit 0
 
+# Options:
+# -Xms 	Sets the initial heap size for when the JVM starts.
+# -Xmx 	Sets the maximum heap size.
+# -Xmn 	Sets the size of the Young Generation.
+# -XX:PermSize 	Sets the starting size of the Permanent Generation.
+# -XX:MaxPermSize 	Sets the maximum size of the Permanent Generation
+#
+# See: 
+# - Java Garbage Collection Basics
+#   http://www.oracle.com/webfolder/technetwork/tutorials/obe/java/gc01/index.html
 
 APPLNAME=javaappl.jar
-java -Xmx12m -Xms3m -Xmn1m -XX:PermSize=20m -XX:MaxPermSize=20m -XX:+UseSerialGC -jar $APPLNAME
+java -Xmx15m -Xms3m -Xmn1m -XX:PermSize=30m -XX:MaxPermSize=30m -XX:+UseSerialGC -jar $APPLNAME
 
